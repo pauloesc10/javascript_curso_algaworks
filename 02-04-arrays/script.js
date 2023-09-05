016c9e5edaa7bd0c8b175bea8c5ef5adc9cd89cf
exago.pcarvalho <paulo@exago.com.br>
2023-09-05 10:30:32 -0300

    Arrays

---
+++ 02-04-arrays/script.js
@@ -0,0 +1,22 @@
+// Declaração de Arrays - iniciam em 0
+var person1 = {
+    name: 'Paulo',
+    age: 31
+}
+
+var person2 = {
+    name: 'Pâmella',
+    age: 31
+}
+
+var person3 = {
+    name: 'João Vitor',
+    age: 1
+}
+
+var list = [person1, person2, person3]
+
+for (var person of list) {
+    console.log(person)
+}
+    
